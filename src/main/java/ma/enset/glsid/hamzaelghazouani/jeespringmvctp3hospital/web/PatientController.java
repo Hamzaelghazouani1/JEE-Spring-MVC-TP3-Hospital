eff01bfce2b0650ebfa79c14f84185b675c2acd8
hamzaelghazouani1 <elghazouani.hamza1@gmail.com>
2025-03-17 15:46:37 +0000

    feat : add pagination

--- src/main/java/ma/enset/glsid/hamzaelghazouani/jeespringmvctp3hospital/web/PatientController.java
+++ src/main/java/ma/enset/glsid/hamzaelghazouani/jeespringmvctp3hospital/web/PatientController.java
@@ -4,9 +4,12 @@ package ma.enset.glsid.hamzaelghazouani.jeespringmvctp3hospital.web;
 import lombok.AllArgsConstructor;
 import ma.enset.glsid.hamzaelghazouani.jeespringmvctp3hospital.entities.Patient;
 import ma.enset.glsid.hamzaelghazouani.jeespringmvctp3hospital.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,9 +19,10 @@ public class PatientController {
     private PatientRepository patientRepository;
 
     @GetMapping("/index")
-    public String index(Model model){
-        List<Patient> patients = patientRepository.findAll();
-        model.addAttribute("patients",patients);
+    public String index(Model model, @RequestParam(name = "page", defaultValue = "0") int page, @RequestParam(name = "size", defaultValue = "1") int size){
+        Page<Patient> patients = patientRepository.findAll(PageRequest.of(page,size));
+        model.addAttribute("patients",patients.getContent());
+        model.addAttribute("pages", new int[patients.getTotalPages()]);
         return "patients";
     }
 }
