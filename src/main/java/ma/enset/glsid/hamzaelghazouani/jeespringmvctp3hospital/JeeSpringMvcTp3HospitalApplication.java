f25614e13fdf6881ecde2e5ae8bc445c18631190
hamzaelghazouani1 <elghazouani.hamza1@gmail.com>
2025-03-17 10:31:03 +0000

    init : Create a JEE Spring MVC project with dependencies

---
+++ src/main/java/ma/enset/glsid/hamzaelghazouani/jeespringmvctp3hospital/JeeSpringMvcTp3HospitalApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.glsid.hamzaelghazouani.jeespringmvctp3hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeSpringMvcTp3HospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeeSpringMvcTp3HospitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/enset/glsid/hamzaelghazouani/jeespringmvctp3hospital/JeeSpringMvcTp3HospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.glsid.hamzaelghazouani.jeespringmvctp3hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeSpringMvcTp3HospitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
